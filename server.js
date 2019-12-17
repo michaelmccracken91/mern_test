c3e934f289e2bfd64ef12030f64b152db44bd777
Michael McCracken <michaelmccracken90@Michaels-MBP.hsd1.az.comcast.net>
2019-12-16 21:20:15 -0700

    initial setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production'){
-    require('dotenv').parse()
+  require('dotenv').config()
 }
+
 
 const express = require('express')
 const app = express()
@@ -16,11 +17,10 @@ app.use(express.static('public'))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true })
+useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
-
 
 app.use('/', indexRouter)
 
